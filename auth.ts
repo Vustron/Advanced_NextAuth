0e7c2d1930b98ff26daa21b3b1bda491f0279cf8
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2024-02-25 14:23:32 +0800

    feat: Added initial callbacks on the middleware

--- auth.ts
+++ auth.ts
@@ -1,7 +1,9 @@
 import { db } from '@/lib/db';
 import NextAuth from 'next-auth';
 import authConfig from '@/auth.config';
+import { UserRole } from '@prisma/client';
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import { getUserById } from '@/lib/actions/getUserById';
 
 export const {
 	handlers: { GET, POST },
@@ -9,6 +11,39 @@ export const {
 	signIn,
 	signOut,
 } = NextAuth({
+	callbacks: {
+		// async signIn({ user }) {
+		// 	const existingUser = await getUserById(user.id!);
+
+		// 	if (!existingUser || !existingUser.emailVerified) {
+		// 		return false;
+		// 	}
+
+		// 	return true;
+		// },
+		async session({ token, session }) {
+			if (token.sub && session.user) {
+				session.user.id = token.sub;
+			}
+
+			if (token.role && session.user) {
+				session.user.role = token.role as UserRole;
+			}
+
+			return session;
+		},
+		async jwt({ token }) {
+			if (!token.sub) return token;
+
+			const existingUser = await getUserById(token.sub);
+
+			if (!existingUser) return token;
+
+			token.role = existingUser.role;
+
+			return token;
+		},
+	},
 	adapter: PrismaAdapter(db),
 	session: { strategy: 'jwt' },
 	...authConfig,
---
+++ next-auth.d.ts
@@ -0,0 +1,19 @@
+import { UserRole } from '@prisma/client';
+import { DefaultSession } from 'next-auth';
+import { JWT } from '@auth/core/jwt';
+
+export type ExtendedUser = DefaultSession['user'] & {
+	role: UserRole;
+};
+
+declare module 'next-auth' {
+	interface Session {
+		user: ExtendedUser;
+	}
+}
+
+declare module '@auth/core/jwt' {
+	interface JWT {
+		role?: 'ADMIN' | 'USER';
+	}
+}
